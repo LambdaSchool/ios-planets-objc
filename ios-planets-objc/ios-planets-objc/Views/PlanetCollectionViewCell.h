5f6b0ffd00a1f69f4d70e67a0ee2d103014ed95d
Conner Alegre <cga0@pm.me>
2018-10-01 14:12:51 -0600

    add view controller for planet cells

---
+++ ios-planets-objc/ios-planets-objc/Views/PlanetCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  PlanetCollectionViewCell.h
+//  ios-planets-objc
+//
+//  Created by Conner on 10/1/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlanetCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
