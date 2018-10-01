946e3be92d1b01a4cf7191c68e7ee74cddf1c7b7
Conner Alegre <cga0@pm.me>
2018-10-01 14:17:03 -0600

    add SettingsViewController with storyboard layout

---
+++ ios-planets-objc/ios-planets-objc/View Controllers/SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  ios-planets-objc
+//
+//  Created by Conner on 10/1/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios-planets-objc/ios-planets-objc/View Controllers/SettingsViewController.m
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.m
+//  ios-planets-objc
+//
+//  Created by Conner on 10/1/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
