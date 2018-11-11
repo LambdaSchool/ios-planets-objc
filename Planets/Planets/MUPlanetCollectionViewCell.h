ddb10d7d8eaa716067c6d4df5951fcdde45c0783
Moin Uddin <U_Moin75@yahoo.com>
2018-11-11 18:31:52 -0500

    Add subclass to custom collection View Cell

---
+++ Planets/Planets/MUPlanetCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MUPlanetCollectionViewCell.h
+//  Planets
+//
+//  Created by Moin Uddin on 11/11/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MUPlanetCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Planets/Planets/MUPlanetCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MUPlanetCollectionViewCell.m
+//  Planets
+//
+//  Created by Moin Uddin on 11/11/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+#import "MUPlanetCollectionViewCell.h"
+
+@implementation MUPlanetCollectionViewCell
+
+@end
