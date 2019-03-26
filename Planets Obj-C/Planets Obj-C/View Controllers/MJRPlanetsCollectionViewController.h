4b8d8831a2d4d814b963ec332d3fbec8068ffafb
Moses Robinson <mosesjrobinson@icloud.com>
2019-03-26 00:29:46 -0700

    collection view implementation done

--- Planets Obj-C/Planets Obj-C/View Controllers/MJRPlanetsCollectionViewController.h
+++ Planets Obj-C/Planets Obj-C/View Controllers/MJRPlanetsCollectionViewController.h
@@ -8,9 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
+@class MJRPlanetController;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MJRPlanetsCollectionViewController : UICollectionViewController
+
+@property MJRPlanetController *planetController;
+@property (readonly) NSArray *planets;
+
 
 @end
 
--- Planets Obj-C/Planets Obj-C/View Controllers/MJRPlanetsCollectionViewController.m
+++ Planets Obj-C/Planets Obj-C/View Controllers/MJRPlanetsCollectionViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "MJRPlanetsCollectionViewController.h"
+#import "MJRPlanetCollectionViewCell.h"
+#import "MJRPlanetController.h"
+#import "MJRPlanet.h"
 
 @interface MJRPlanetsCollectionViewController ()
 
@@ -14,80 +17,52 @@
 
 @implementation MJRPlanetsCollectionViewController
 
-static NSString * const reuseIdentifier = @"Cell";
+static NSString * const reuseIdentifier = @"PlanetCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
     // Register cell classes
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+//    [self.collectionView registerClass:[MJRPlanetCollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
     
-    // Do any additional setup after loading the view.
+    if (self) {
+        _planetController = [[MJRPlanetController alloc] init];
+    }
+    return self;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 #pragma mark <UICollectionViewDataSource>
 
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
-
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of items
-    return 0;
+
+    return [[self planets] count];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    MJRPlanetCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
     
-    // Configure the cell
+    MJRPlanet *planet = [[self planets] objectAtIndex: [indexPath row]];
+    
+    [[cell planetLabel] setText: [planet name]];
+    [[cell planetImage] setImage: [planet image]];
     
     return cell;
 }
 
-#pragma mark <UICollectionViewDelegate>
-
-/*
-// Uncomment this method to specify if the specified item should be highlighted during tracking
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
-	return YES;
+- (NSArray *)planets {
+    
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    BOOL shouldShowPluto = [userDefaults boolForKey: @"ShouldShowPluto"];
+    
+    if (shouldShowPluto) {
+        return [_planetController planetsPlusPluto];
+    }
+    return [_planetController planetsMinusPluto];
 }
-*/
-
-/*
-// Uncomment this method to specify if the specified item should be selected
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    return YES;
-}
-*/
-
-/*
-// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
-	return NO;
-}
-
-- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	return NO;
-}
-
-- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	
-}
-*/
 
 @end
