fa27870e5b7e5549818b64839fc2dd1c13853ef8
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-01 15:04:34 -0400

    create class for collection view cell

---
+++ Planets/Planets/GLIPlanetsCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  GLIPlanetsCollectionViewCell.h
+//  Planets
+//
+//  Created by Julian A. Fordyce on 4/1/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GLIPlanetsCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Planets/Planets/GLIPlanetsCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  GLIPlanetsCollectionViewCell.m
+//  Planets
+//
+//  Created by Julian A. Fordyce on 4/1/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import "GLIPlanetsCollectionViewCell.h"
+
+@implementation GLIPlanetsCollectionViewCell
+
+@end
