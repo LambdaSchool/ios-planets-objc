129b82f1e3c4378c2892f07b298b30b1235401ef
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-01 19:54:17 -0400

    build UI, made connections and set classes

---
+++ Planets/Planets/GLISettingsViewController.m
@@ -0,0 +1,32 @@
+//
+//  GLISettingsViewController.m
+//  Planets
+//
+//  Created by Julian A. Fordyce on 4/1/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import "GLISettingsViewController.h"
+
+@interface GLISettingsViewController ()
+
+@end
+
+@implementation GLISettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
