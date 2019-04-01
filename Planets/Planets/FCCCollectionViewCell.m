66501c2f872766eba6f106329556393c4aba75f4
Frulwinn <frulwinn@gmail.com>
2019-04-01 16:26:04 -0500

    set up storyboard and classed view controllers

---
+++ Planets/Planets/FCCCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  FCCCollectionViewCell.m
+//  Planets
+//
+//  Created by Lambda_School_Loaner_34 on 4/1/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+#import "FCCCollectionViewCell.h"
+
+@implementation FCCCollectionViewCell
+
+@end
