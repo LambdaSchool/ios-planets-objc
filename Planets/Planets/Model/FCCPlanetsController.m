97f5b0640b921af37823d6da1d1a94eb3247331a
Frulwinn <frulwinn@gmail.com>
2019-04-01 17:27:18 -0500

    added planet assets

---
+++ Planets/Planets/Model/FCCPlanetsController.m
@@ -0,0 +1,26 @@
+//
+//  FCCPlanetsController.m
+//  Planets
+//
+//  Created by Lambda_School_Loaner_34 on 4/1/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+#import "FCCPlanetsController.h"
+#import "FCCPlanets.h"
+
+@interface FCCPlanetsController ()
+
+    - (void)printTotalVersion;
+
+@implementation FCCPlanetsController
+
+- (instancetype)init {
+    self = [super init];
+    if (self != nil) {
+        
+        _planets = 
+    }
+}
+
+@end
