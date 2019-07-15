cbbc697eb3ae07777a72d4822450a4e2fbb79956
Kobe McKee <kobe.mckee@gmail.com>
2019-07-15 14:12:10 -0600

    Part 1 complete

---
+++ OBJCPlanets/OBJCPlanets/KTMSettingsViewController.h
@@ -0,0 +1,20 @@
+//
+//  KTMSettingsViewController.h
+//  OBJCPlanets
+//
+//  Created by Kobe McKee on 7/15/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KTMSettingsViewController : UIViewController
+
+- (IBAction)donePressed:(UIButton *)sender;
+- (IBAction)togglePluto:(UISwitch *)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
