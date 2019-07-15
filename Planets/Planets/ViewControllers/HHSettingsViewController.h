a04959752273015efe3db05faaac7f33a0840583
Hayden Hastings <haydenhastings32@gmail.com>
2019-07-15 14:50:34 -0600

    Added a key for the switch for Pluto.

--- Planets/Planets/ViewControllers/HHSettingsViewController.h
+++ Planets/Planets/ViewControllers/HHSettingsViewController.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface HHSettingsViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UISwitch *shouldShowPlutoSwitch;
+
 - (IBAction)switchButtonTapped:(UISwitch *)sender;
 - (IBAction)doneButtonTapped:(UIButton *)sender;
 @end
--- Planets/Planets/ViewControllers/HHSettingsViewController.m
+++ Planets/Planets/ViewControllers/HHSettingsViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    _shouldShowPlutoSwitch.on = [userDefaults boolForKey:@"shouldShowPluto"];
 }
 
 /*
