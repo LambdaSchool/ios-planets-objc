b361f8a4b7e2eb712806b3fd82d3a73b763f2736
Christopher Aronson <christopheraronson@icloud.com>
2019-07-15 12:24:39 -0700

    Added .gitignore

---
+++ Planets/Planets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Planets
+//
+//  Created by Christopher Aronson on 7/15/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Planets/Planets/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Planets
+//
+//  Created by Christopher Aronson on 7/15/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
