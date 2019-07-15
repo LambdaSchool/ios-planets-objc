161b0fc436ed8f662e1e50e48429100d28b298ff
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-15 16:18:12 -0700

    controller setup!!

--- Planets/Planets/Model Controller/LSIPlanetController.h
+++ Planets/Planets/Model Controller/LSIPlanetController.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSIPlanetController : NSObject
 
+@property NSArray *planets;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Planets/Planets/Model Controller/LSIPlanetController.m
+++ Planets/Planets/Model Controller/LSIPlanetController.m
@@ -5,9 +5,27 @@
 //  Created by Hector Steven on 7/15/19.
 //  Copyright © 2019 Hector Steven. All rights reserved.
 //
-
+#import <UIKit/UIKit.h>
 #import "LSIPlanetController.h"
+#import "../Model/LSIPlanets.h"
 
 @implementation LSIPlanetController
-
+-(instancetype)init {
+	self = [super init];
+	if (self) {
+		_planets = @[
+					 [[LSIPlanets alloc] initWithPlanetName:@"Mercury" planetImage: [UIImage imageNamed:@"mercury"]],
+					 [[LSIPlanets alloc] initWithPlanetName:@"Venus" planetImage: [UIImage imageNamed:@"venus"]],
+					 [[LSIPlanets alloc] initWithPlanetName:@"Earth" planetImage: [UIImage imageNamed:@"earth"]],
+					 [[LSIPlanets alloc] initWithPlanetName:@"Mars" planetImage: [UIImage imageNamed:@"mars"]],
+					 [[LSIPlanets alloc] initWithPlanetName:@"Jupiter" planetImage: [UIImage imageNamed:@"jupiter"]],
+					 [[LSIPlanets alloc] initWithPlanetName:@"Saturn" planetImage: [UIImage imageNamed:@"saturn"]],
+					 [[LSIPlanets alloc] initWithPlanetName:@"Uranus" planetImage: [UIImage imageNamed:@"uranus"]],
+					 [[LSIPlanets alloc] initWithPlanetName:@"Neptune" planetImage: [UIImage imageNamed:@"neptune"]]
+					 
+					 
+					 ];
+	}
+	return self;
+}
 @end
