1c978d1a9edf07f8eba51d310c0f8f7e2b425e1e
Michael R.F <mrflowers22@uwalumni.com>
2019-07-15 13:45:58 -0700

    implemented the model and the initializer

---
+++ OBJCPlanetsHW/OBJCPlanetsHW/MRFPlanet.h
@@ -0,0 +1,20 @@
+//
+//  MRFPlanet.h
+//  OBJCPlanetsHW
+//
+//  Created by Michael Flowers on 7/15/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MRFPlanet : NSObject
+//create the properties for the model
+@property NSString *planetName;
+@property NSString *imageName;
+
+@end
+
+NS_ASSUME_NONNULL_END
