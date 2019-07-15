56a1b9b9266e5ac91dfa29ddf5489309e6bd82db
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-15 13:24:08 -0700

    collection view

---
+++ Planets/Planets/View Controllers/LSIPlanetsCollectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  LSIPlanetsCollectionViewController.h
+//  Planets
+//
+//  Created by Hector Steven on 7/15/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIPlanetsCollectionViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
