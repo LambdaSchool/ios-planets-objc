7b995837710ea36ffeae45a1ec9370925762a487
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-07-16 09:13:45 -0400

    create model controller

---
+++ Planets/Planets/GLIPlanetsController.h
@@ -0,0 +1,21 @@
+//
+//  GLIPlanetsController.h
+//  Planets
+//
+//  Created by Julian A. Fordyce on 7/16/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GLIPlanetsController : NSObject
+
+@property NSArray *planets;
+@property NSArray *plantetsWithoutPluto;
+@property NSArray *planetsWithPluto;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Planets/Planets/GLIPlanetsController.m
@@ -0,0 +1,13 @@
+//
+//  GLIPlanetsController.m
+//  Planets
+//
+//  Created by Julian A. Fordyce on 7/16/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import "GLIPlanetsController.h"
+
+@implementation GLIPlanetsController
+
+@end
