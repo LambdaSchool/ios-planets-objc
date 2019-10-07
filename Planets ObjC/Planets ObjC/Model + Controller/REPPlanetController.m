b346fabde5d4675cdb9db671ef31f7249f360eaf
Michael Redig <mredig@gmail.com>
2019-10-07 16:08:41 -0500

    added model controller organize

---
+++ Planets ObjC/Planets ObjC/Model + Controller/REPPlanetController.m
@@ -0,0 +1,13 @@
+//
+//  REPPlanetController.m
+//  Planets ObjC
+//
+//  Created by Michael Redig on 10/7/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import "REPPlanetController.h"
+
+@implementation REPPlanetController
+
+@end
