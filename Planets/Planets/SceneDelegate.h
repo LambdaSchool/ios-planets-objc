6bbbb27615ad551f010c3538b5870ec9c0e36743
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-10-30 21:07:57 -0400

    Added Project and images to project

---
+++ Planets/Planets/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Planets
+//
+//  Created by Michael Stoffer on 10/30/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Planets/Planets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Planets
+//
+//  Created by Michael Stoffer on 10/30/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
