c62d515b2baca96b53b295cc1b332aea40a7a217
Isvvc <lyons@tuta.io>
2019-12-09 12:54:19 -0700

    Create blank project

---
+++ Planets Obj-C/Planets Obj-C/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Planets Obj-C
+//
+//  Created by Isaac Lyons on 12/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Planets Obj-C/Planets Obj-C/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Planets Obj-C
+//
+//  Created by Isaac Lyons on 12/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
