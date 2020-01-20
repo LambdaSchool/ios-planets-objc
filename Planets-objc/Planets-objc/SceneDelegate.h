a0b29d709211a0be18fe7c9a0c1ad699f47fffa9
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-19 17:34:06 -0800

    Base planets obj-c project

---
+++ Planets-objc/Planets-objc/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Planets-objc
+//
+//  Created by Lambda_School_Loaner_204 on 1/19/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
