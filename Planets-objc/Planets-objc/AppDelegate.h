afc13b245065717decbbae820866609ba9a7b7f0
John Kouris <jdkouris@gmail.com>
2020-01-29 18:12:54 -0800

    Added the skeleton project

---
+++ Planets-objc/Planets-objc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Planets-objc
+//
+//  Created by John Kouris on 1/29/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Planets-objc/Planets-objc/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Planets-objc
+//
+//  Created by John Kouris on 1/29/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
